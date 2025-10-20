cab1d1fbdc729dc366277f001563af18bcaec7ee
Hugo <hugo.pointier@gmail.com>
2025-10-20 13:42:04 +0200

    added weapon state

--- backend/src/weapon.rs
+++ backend/src/weapon.rs
@@ -1,9 +1,44 @@
+//TODO: add enum for type of weapon or way to handle it
 pub struct Weapon {
     base_damage: f64,
     boon_damage: f64,
     fire_rate: f64,
     reload_time: f64,
     mag_size: i32,
+}
+
+pub struct WeaponState {
+    pub boon_level: i32,
+    pub bonus_damage: i32,
+    pub bonus_fire_rate: i32,
+    pub bonus_ammo: i32,
+}
+
+impl WeaponState {
+    pub fn default() -> WeaponState {
+        WeaponState {
+            boon_level: 0,
+            bonus_damage: 0,
+            bonus_fire_rate: 0,
+            bonus_ammo: 0,
+        }
+    }
+    pub fn new(
+        boon_level: i32,
+        bonus_damage: i32,
+        bonus_fire_rate: i32,
+        bonus_ammo: i32,
+    ) -> WeaponState {
+        if boon_level < 0 {
+            panic!("Negative value for boon level {}", boon_level)
+        }
+        WeaponState {
+            boon_level,
+            bonus_damage,
+            bonus_fire_rate,
+            bonus_ammo,
+        }
+    }
 }
 
 impl Weapon {
@@ -34,18 +69,19 @@ impl Weapon {
         }
     }
 
-    pub fn current_damage(&self, boon_level: i32) -> f64 {
-        self.base_damage + boon_level as f64 * self.boon_damage
+    pub fn current_damage(&self, state: &WeaponState) -> f64 {
+        let percentage_damage :f64 = 1.0 + state.bonus_damage as f64 / 100.0;
+        (self.base_damage + state.boon_level as f64 * self.boon_damage) * percentage_damage
     }
-}
 
-fn current_fire_rate(base_fire_rate: f64, bonus_fire_rate: i32) -> f64 {
-    let percentage_fire_rate: f64 = 1.0 + bonus_fire_rate as f64 / 100.0;
-    base_fire_rate * percentage_fire_rate
-}
+    pub fn current_fire_rate(&self, bonus_fire_rate: i32) -> f64 {
+        let percentage_fire_rate: f64 = 1.0 + bonus_fire_rate as f64 / 100.0;
+        self.fire_rate * percentage_fire_rate
+    }
 
-fn dps_no_reload(damage: f64, fire_rate: f64) -> f64 {
-    damage * fire_rate
+    pub fn dps_no_reload(&self, bonus_damage: i32) -> f64 {
+        0.0
+    }
 }
 
 #[cfg(test)]
@@ -67,28 +103,74 @@ mod tests {
     #[test]
     #[should_panic]
     fn weapon_new_panic_negative_value() {
-        let weapon = Weapon::new(-5.0, 0.0, 3.0, 4.0, 20);
+        let _weapon = Weapon::new(-5.0, 0.0, 3.0, 4.0, 20);
+    }
+
+    #[test]
+    fn weapon_state_default(){
+        let state = WeaponState::default();
+        assert_eq!(state.boon_level,0);
+        
+        assert_eq!(state.bonus_damage,0);
+        assert_eq!(state.bonus_fire_rate,0);
+        assert_eq!(state.bonus_ammo,0);
+    }
+    #[test]
+    fn weapon_state_new() {
+        let state = WeaponState::new(5, 50, 100, 0);
+        assert_eq!(state.boon_level, 5);
+        assert_eq!(state.bonus_damage, 50);
+        assert_eq!(state.bonus_fire_rate, 100);
+        assert_eq!(state.bonus_ammo, 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn weapon_state_new_boon_panic() {
+        let _state = WeaponState::new(-1, 10, 10, 10);
+    }
+
+    #[test]
+    fn weapon_default_current_damage() {
+        let weapon = Weapon::new(10.0, 0.5, 2.5, 2.0, 15);
+        let state = WeaponState::default();
+        assert_relative_eq!(weapon.current_damage(&state), 10.0);
+    }
+
+    #[test]
+    fn weapon_current_damage_bonus_damage(){
+        let weapon = Weapon::new(10.0, 0.5, 2.5, 2.0, 15);
+        let state = WeaponState{bonus_damage:50, ..WeaponState::default()};
+        assert_relative_eq!(weapon.current_damage(&state),15.0)
+
+    }
+
+    #[test]
+    fn weapon_current_damage_boon_level(){
+
+        let weapon = Weapon::new(10.0, 0.5, 2.5, 2.0, 15);
+        let state = WeaponState{boon_level:5, ..WeaponState::default()};
+        assert_relative_eq!(weapon.current_damage(&state),12.5)
     }
 
     #[test]
     fn weapon_current_fire_rate() {
         let weapon = Weapon::new(10.0, 0.5, 2.5, 2.0, 15);
-        let boon_level = 5;
-        assert_relative_eq!(weapon.current_damage(boon_level), 12.5);
+        let bonus_fire_rate = 100;
+        assert_relative_eq!(weapon.current_fire_rate(bonus_fire_rate), 5.0);
     }
 
     #[test]
-    fn test_dps_no_reload() {
-        assert_relative_eq!(dps_no_reload(5.0, 3.0), 15.0);
+    fn weapon_negative_fire_rate() {
+        let weapon = Weapon::new(10.0, 0.5, 2.5, 2.0, 15);
+        let bonus_fire_rate = -40;
+        assert_relative_eq!(weapon.current_fire_rate(bonus_fire_rate), 1.5)
     }
 
     #[test]
-    fn test_current_fire_rate() {
-        assert_relative_eq!(current_fire_rate(2.0, 50), 3.0)
-    }
-
-    #[test]
-    fn negative_fire_rate() {
-        assert_relative_eq!(current_fire_rate(10.0, -40), 6.0)
+    fn weapon_dps_no_reload() {
+        let weapon = Weapon::new(10.0, 0.5, 2.5, 2.0, 15);
+        let bonus_damage = 50;
+        let boon_level = 0.5;
     }
 }
