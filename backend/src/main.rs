4f616825b2217209d16c55f51235cbda3c90458f
Hugo <hugo.pointier@gmail.com>
2025-10-24 22:10:58 +0200

    starting to parse item json using serde

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,23 @@
-mod weapon;
-fn main() {
+//TODO: add weap/dur/spirit threshold as hard coded value here ?
+use std::{collections::HashMap, fs};
+use serde::Deserialize;
+use serde_json;
+
+#[derive(Debug,Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct Item{
+    name:Option<String>,
+    description:Option<String>,
+    cost:Option<i32>,
 }
 
+pub type ItemMap = HashMap<String, Item>;
+mod weapon;
+fn main() {
+    let item_data = fs::read_to_string("/home/demonz/programmation/Project_Lock/data/item-data.json").expect("Failed to read file");
+    let items :ItemMap = serde_json::from_str(&item_data).expect("Cant read data");
+
+
+    println!("{:#?}", items.get("upgrade_ability_refresher").unwrap().name);
+
+}
--- backend/src/weapon.rs
+++ backend/src/weapon.rs
@@ -16,12 +16,16 @@ pub struct Weapon {
     max_falloff: f64, // In meter
 }
 
+
+//TODO: maybe do a character state for information like boon level and dist from enemy or even
+//enemy state
 pub struct WeaponState {
     pub boon_level: i32,
     pub bonus_damage: i32,
     pub bonus_fire_rate: i32,
     pub bonus_ammo: i32,
     pub bonus_falloff: i32,
+    pub dist:f64,
 }
 
 impl WeaponState {
@@ -32,6 +36,7 @@ impl WeaponState {
             bonus_fire_rate: 0,
             bonus_ammo: 0,
             bonus_falloff: 0,
+            dist:0.0,
         }
     }
     pub fn new(
@@ -40,6 +45,7 @@ impl WeaponState {
         bonus_fire_rate: i32,
         bonus_ammo: i32,
         bonus_falloff: i32,
+        dist:f64,
     ) -> WeaponState {
         if boon_level < 0 {
             panic!("Negative value for boon level {}", boon_level)
@@ -50,6 +56,7 @@ impl WeaponState {
             bonus_fire_rate,
             bonus_ammo,
             bonus_falloff,
+            dist,
         }
     }
 }
@@ -114,28 +121,23 @@ impl Weapon {
         self.fire_rate * percentage_fire_rate
     }
     pub fn dps_no_reload(&self, state: &WeaponState) -> f64 {
-        let percent_damage = 1.0 + state.bonus_damage as f64 / 100.0;
-        let percent_fire_rate = 1.0 + state.bonus_fire_rate as f64 / 100.0;
-        (self.base_damage + self.boon_damage * state.boon_level as f64)
-            * percent_damage
-            * self.fire_rate
-            *percent_fire_rate
+        self.current_damage(state) * self.current_fire_rate(state) * self.falloff_damage(state)
     }
-    pub fn falloff_damage(&self, dist: f64, state: &WeaponState) -> f64 {
+    pub fn falloff_damage(&self,  state: &WeaponState) -> f64 {
         //Linear Interpolation:
         let min_falloff = self.min_falloff * (1.0 + state.bonus_falloff as f64 / 100.0);
 
         let max_falloff = self.max_falloff * (1.0 + state.bonus_falloff as f64 / 100.0);
         let min_mult = 0.1;
         let max_mult = 1.0;
-        if dist < min_falloff {
+        if state.dist < min_falloff {
             max_mult
-        } else if dist > max_falloff {
+        } else if state.dist > max_falloff {
             min_mult
         } else {
             let range = max_mult - min_mult;
             let max_damage = 1.0;
-            let factor = (dist - min_falloff) / (max_falloff - min_falloff);
+            let factor = (state.dist - min_falloff) / (max_falloff - min_falloff);
             max_damage - range * factor
         }
         // damage range between no falloff and max falloff
@@ -175,21 +177,23 @@ mod tests {
         assert_eq!(state.bonus_damage, 0);
         assert_eq!(state.bonus_fire_rate, 0);
         assert_eq!(state.bonus_ammo, 0);
+        assert_relative_eq!(state.dist, 0.0)
     }
     #[test]
     fn weapon_state_new() {
-        let state = WeaponState::new(5, 50, 100, 0, 0);
+        let state = WeaponState::new(5, 50, 100, 0, 0, 10.0);
         assert_eq!(state.boon_level, 5);
         assert_eq!(state.bonus_damage, 50);
         assert_eq!(state.bonus_fire_rate, 100);
         assert_eq!(state.bonus_ammo, 0);
-        assert_eq!(state.bonus_falloff, 0)
+        assert_eq!(state.bonus_falloff, 0);
+        assert_relative_eq!(state.dist, 10.0)
     }
 
     #[test]
     #[should_panic]
     fn weapon_state_new_boon_panic() {
-        let _state = WeaponState::new(-1, 10, 10, 10, 0);
+        let _state = WeaponState::new(-1, 10, 10, 10, 0, 0.0);
     }
 
     #[test]
@@ -202,22 +206,22 @@ mod tests {
     #[test]
     fn weapon_falloff_damage() {
         let weapon = Weapon::new(10.0, 0.5, 2.5, 2.0, 15, WeaponType::Auto, 12.0, 24.0);
-        let state = WeaponState::default();
-        assert_relative_eq!(weapon.falloff_damage(16.0, &state), 0.7);
+        let state = WeaponState{dist:16.0, ..WeaponState::default()};
+        assert_relative_eq!(weapon.falloff_damage( &state), 0.7);
     }
 
     #[test]
     fn weapon_min_falloff() {
         let weapon = Weapon::new(10.0, 0.5, 2.5, 2.0, 15, WeaponType::Auto, 12.0, 24.0);
-        let state = WeaponState::default();
-        assert_relative_eq!(weapon.falloff_damage(5.0, &state), 1.0);
+        let state = WeaponState{dist:5.0, ..WeaponState::default()};
+        assert_relative_eq!(weapon.falloff_damage( &state), 1.0);
     }
 
     #[test]
     fn weapon_max_falloff() {
         let weapon = Weapon::new(10.0, 0.5, 2.5, 2.0, 15, WeaponType::Auto, 12.0, 24.0);
-        let state = WeaponState::default();
-        assert_relative_eq!(weapon.falloff_damage(30.0, &state), 0.1)
+        let state = WeaponState{dist:32.0, ..WeaponState::default()};
+        assert_relative_eq!(weapon.falloff_damage( &state), 0.1)
     }
     #[test]
     fn weapon_current_damage_bonus_damage() {
