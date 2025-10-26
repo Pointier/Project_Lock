ec2bad8284d0091766b38cc3e379c7c1f28b6e5a
Hugo <hugo.pointier@gmail.com>
2025-10-26 17:52:14 +0100

    check to see if an item is valid

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,31 +1,36 @@
 //TODO: add weap/dur/spirit threshold as hard coded value here ?
 use serde::Deserialize;
 use std::{collections::HashMap, fs};
+mod weapon;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
-pub struct Item {
+pub struct ParsedItem {
     name: Option<String>,
     description: Option<String>,
     cost: Option<i32>,
     slot: Option<String>,
     bonus_fire_rate: Option<i32>,
+    is_disabled:Option<bool>,
 }
 
-pub type ItemMap = HashMap<String, Item>;
+fn valid_item(item:ParsedItem)->bool{
+    item.name.is_some() && !item.is_disabled.expect("Missing value for is_disabled field (item).")
 
-fn parse_items(json: &str) -> ItemMap {
-     serde_json::from_str(json).expect("Cant read data")
 }
-mod weapon;
+pub type ParsedItemMap = HashMap<String, ParsedItem>;
+
+fn parse_items(json: &str) -> ParsedItemMap {
+     serde_json::from_str(json).expect("Cant read item json")
+}
+
+fn item_vec(items: ParsedItemMap)->Vec<ParsedItem>{
+
+}
 fn main() {
     let path_data = "/home/demonz/programmation/Project_Lock/data/item-data.json";
     let item_data = fs::read_to_string(path_data).expect("Failed to read file");
-    let items: ItemMap = serde_json::from_str(&item_data).expect("Cant read data");
-    println!(
-        "{:#?}",
-        items.get("upgrade_ability_refresher").unwrap().name
-    );
+    let items: ParsedItemMap = serde_json::from_str(&item_data).expect("Cant read data");
 }
 
 #[cfg(test)]
@@ -63,7 +68,7 @@ mod test {
                 "BonusFireRate": 20
             }
         }"#;
-        let items: ItemMap = parse_items(data);
+        let items: ParsedItemMap = parse_items(data);
         let item = items.get("upgrade_ricochet").unwrap();
         assert_eq!(item.name.as_ref().unwrap(), "Ricochet");
         assert_eq!(
@@ -72,6 +77,66 @@ mod test {
         );
         assert_eq!(item.cost.unwrap(), 6400);
         assert_eq!(item.slot.as_ref().unwrap(),"Weapon");
-        assert_eq!(item.bonus_fire_rate.unwrap(), 20)
+        assert_eq!(item.bonus_fire_rate.unwrap(), 20);
+        assert!(!item.is_disabled.unwrap())
+    }
+
+    #[test]
+    fn item_is_valid(){
+        let data = r#"
+        {
+            "Name": "Echo Shard",
+            "Description": null,
+            "Cost": 6400,
+            "Tier": 4,
+            "Activation": "InstantCast",
+            "Slot": "Tech",
+            "Components": null,
+            "TargetTypes": null,
+            "ShopFilters": [
+                "Movement",
+                "MagicDamage"
+            ],
+            "IsDisabled": false,
+            "AbilityCooldown": 30.0,
+            "AbilityUnitTargetLimit": 1,
+            "AbilityCastDelay": 0.2,
+            "ChannelMoveSpeed": -1,
+            "CooldownReduction": 5,
+            "BonusFireRate": 5,
+            "TechResist": 5,
+            "BulletResist": 5
+        }
+            "#;
+        let item :ParsedItem= serde_json::from_str(data).unwrap();
+        // item is valid if have a name and IsDisabled is false
+        assert!(valid_item(item), "Item wasnt valid");
+
+    }
+    #[test]
+    fn item_is_not_valid(){
+        let data= r#"
+        {
+            "Name": null,
+            "Description": null,
+            "Cost": 9999,
+            "Tier": 5,
+            "Activation": "Passive",
+            "Slot": "Tech",
+            "Components": null,
+            "TargetTypes": null,
+            "ShopFilters": null,
+            "IsDisabled": false,
+            "AbilityUnitTargetLimit": 1,
+            "ChannelMoveSpeed": -1
+        }
+            "#;
+        let item :ParsedItem= serde_json::from_str(data).unwrap();
+        assert!(!valid_item(item), "Item was valid");
+    }
+    #[test]
+    fn parsed_item_vector() {
+        let data  = r#""#;
+
     }
 }
